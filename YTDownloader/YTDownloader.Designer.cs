7832919b6066c446eeaf63d4557a8f1bc68ed9f3
LS91 <ls.sadowski@gmail.com>
2018-02-21 22:17:18 +0100

    Add project files.

---
+++ YTDownloader/YTDownloader.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace YTDownloader
+{
+    partial class YTDownloader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(YTDownloader));
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.urlLabel = new System.Windows.Forms.Label();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.chooseLabel = new System.Windows.Forms.Label();
+            this.chooseTextBox = new System.Windows.Forms.TextBox();
+            this.chooseButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.pasteButton = new System.Windows.Forms.Button();
+            this.stateLabel = new System.Windows.Forms.Label();
+            this.folderExplorerButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.downloadButton.Location = new System.Drawing.Point(12, 84);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(479, 40);
+            this.downloadButton.TabIndex = 0;
+            this.downloadButton.Text = "Ściągnij";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_ClickAsync);
+            // 
+            // urlLabel
+            // 
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.Location = new System.Drawing.Point(12, 30);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Size = new System.Drawing.Size(80, 17);
+            this.urlLabel.TabIndex = 1;
+            this.urlLabel.Text = "Podaj URL:";
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Enabled = false;
+            this.urlTextBox.Location = new System.Drawing.Point(124, 27);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(449, 22);
+            this.urlTextBox.TabIndex = 2;
+            this.urlTextBox.TextChanged += new System.EventHandler(this.urlTextBox_TextChanged);
+            // 
+            // chooseLabel
+            // 
+            this.chooseLabel.AutoSize = true;
+            this.chooseLabel.Location = new System.Drawing.Point(12, 59);
+            this.chooseLabel.Name = "chooseLabel";
+            this.chooseLabel.Size = new System.Drawing.Size(103, 17);
+            this.chooseLabel.TabIndex = 3;
+            this.chooseLabel.Text = "Wybierz folder:";
+            // 
+            // chooseTextBox
+            // 
+            this.chooseTextBox.Enabled = false;
+            this.chooseTextBox.Location = new System.Drawing.Point(124, 56);
+            this.chooseTextBox.Name = "chooseTextBox";
+            this.chooseTextBox.Size = new System.Drawing.Size(449, 22);
+            this.chooseTextBox.TabIndex = 4;
+            // 
+            // chooseButton
+            // 
+            this.chooseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.chooseButton.Location = new System.Drawing.Point(577, 54);
+            this.chooseButton.Name = "chooseButton";
+            this.chooseButton.Size = new System.Drawing.Size(82, 27);
+            this.chooseButton.TabIndex = 5;
+            this.chooseButton.Text = "Wybierz";
+            this.chooseButton.UseVisualStyleBackColor = true;
+            this.chooseButton.Click += new System.EventHandler(this.chooseButton_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(15, 130);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(644, 23);
+            this.progressBar.TabIndex = 6;
+            // 
+            // pasteButton
+            // 
+            this.pasteButton.Location = new System.Drawing.Point(577, 25);
+            this.pasteButton.Name = "pasteButton";
+            this.pasteButton.Size = new System.Drawing.Size(82, 27);
+            this.pasteButton.TabIndex = 7;
+            this.pasteButton.Text = "Wklej";
+            this.pasteButton.UseVisualStyleBackColor = true;
+            this.pasteButton.Click += new System.EventHandler(this.pasteButton_Click);
+            // 
+            // stateLabel
+            // 
+            this.stateLabel.AutoSize = true;
+            this.stateLabel.Location = new System.Drawing.Point(12, 9);
+            this.stateLabel.Name = "stateLabel";
+            this.stateLabel.Size = new System.Drawing.Size(0, 17);
+            this.stateLabel.TabIndex = 8;
+            // 
+            // folderExplorerButton
+            // 
+            this.folderExplorerButton.Location = new System.Drawing.Point(497, 84);
+            this.folderExplorerButton.Name = "folderExplorerButton";
+            this.folderExplorerButton.Size = new System.Drawing.Size(162, 40);
+            this.folderExplorerButton.TabIndex = 9;
+            this.folderExplorerButton.Text = "Otwórz folder z muzą";
+            this.folderExplorerButton.UseVisualStyleBackColor = true;
+            this.folderExplorerButton.Click += new System.EventHandler(this.folderExplorerButton_Click);
+            // 
+            // YTDownloader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 161);
+            this.Controls.Add(this.folderExplorerButton);
+            this.Controls.Add(this.stateLabel);
+            this.Controls.Add(this.pasteButton);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.chooseButton);
+            this.Controls.Add(this.chooseTextBox);
+            this.Controls.Add(this.chooseLabel);
+            this.Controls.Add(this.urlTextBox);
+            this.Controls.Add(this.urlLabel);
+            this.Controls.Add(this.downloadButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "YTDownloader";
+            this.Text = "YTDownloader";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.YTDownloader_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.Label urlLabel;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Label chooseLabel;
+        private System.Windows.Forms.TextBox chooseTextBox;
+        private System.Windows.Forms.Button chooseButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button pasteButton;
+        private System.Windows.Forms.Label stateLabel;
+        private System.Windows.Forms.Button folderExplorerButton;
+    }
+}
+
---
+++ YTDownloader/YTDownloader.cs
@@ -0,0 +1,228 @@
+﻿using CliWrap;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExplode;
+using YoutubeExplode.Models.MediaStreams;
+
+namespace YTDownloader
+{
+    public partial class YTDownloader : Form
+    {
+        private static readonly Cli FfmpegCli = new Cli("ffmpeg.exe");
+        private static readonly string TempDirectoryPath = Path.Combine(Directory.GetCurrentDirectory(), "Temp");
+        private readonly YoutubeClient YoutubeClient = new YoutubeClient();
+        private readonly string startedFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Muza z Youtube'a";
+        private List<string> errorList = new List<string>();
+
+        public YTDownloader()
+        {
+            InitializeComponent();
+            SetStartedFolder();
+            stateLabel.Text = "Wklej link do filmu z YouTube'a";
+        }
+
+        private void SetStartedFolder()
+        {
+            chooseTextBox.Text = startedFolder;
+        }
+
+        private void chooseButton_Click(object sender, EventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                folderDialog.SelectedPath = desktopPath;
+
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    chooseTextBox.Text = folderDialog.SelectedPath;
+                }
+            }
+        }
+
+        private async void downloadButton_ClickAsync(object sender, EventArgs e)
+        {
+            downloadButton.Enabled = false;
+            // Try to determine the type of the URL/ID that was given
+
+            // Playlist URL
+            if (YoutubeClient.TryParsePlaylistId(urlTextBox.Text, out var playlistId))
+            {
+                await SavePlaylist(playlistId);
+            }
+
+            // Video URL
+            else if (YoutubeClient.TryParseVideoId(urlTextBox.Text, out var videoId))
+            {
+                try
+                {
+                    await SaveTrackAudioAsync(videoId, -1, -1);
+                }
+                catch
+                {
+
+                    MessageBox.Show($"Niestety wystąpił błąd :( Z utworu: {videoId} nic nie będzie :(");
+                }
+                
+            }
+
+            // Unknown
+            else
+            {
+                MessageBox.Show("Błędny adres URL :( Wklej poprawny link!");
+            }
+
+            downloadButton.Enabled = true;
+            urlTextBox.Text = "";
+            stateLabel.Text = "Udało się! Wklej kolejny link :)";
+        }
+
+        private async Task SaveTrackAudioAsync(string id, int index, int all, string playlistTitle = "",  bool isFromPlaylist=false)
+        {
+            if (index != -1)
+                stateLabel.Text = $"Ściągam playlistę. Aktualnie utwór nr {index} z {all}";
+            else
+                stateLabel.Text = $"Ściągam utwór...";
+
+            var url = urlTextBox.Text;
+            var video = await YoutubeClient.GetVideoAsync(id);
+
+            //replace invalid character for '_'
+            var cleanTitle = video.Title;
+            for (int i = 0; i < cleanTitle.Length -1; i++)
+            {
+                if (Path.GetInvalidFileNameChars().Contains(cleanTitle[i]))
+                    cleanTitle = cleanTitle.Replace(cleanTitle[i], ' ');
+            }
+
+            var streamInfoSet = await YoutubeClient.GetVideoMediaStreamInfosAsync(id);
+            var streamInfo = streamInfoSet.Audio.WithHighestBitrate();
+
+            //temp
+            Directory.CreateDirectory(TempDirectoryPath);
+            var streamFileExt = streamInfo.Container.GetFileExtension();
+            var streamFilePath = Path.Combine(TempDirectoryPath, $"{Guid.NewGuid()}.{streamFileExt}");
+            var progressHandler = new Progress<double>(p => progressBar.Value = (int)p * 100);
+            await YoutubeClient.DownloadMediaStreamAsync(streamInfo, streamFilePath, progressHandler);
+
+            // Convert to mp3
+            stateLabel.Text = "Konwertuję utwór na mp3...";
+            string directory = chooseTextBox.Text;
+            if (playlistTitle != "")
+                directory += "\\" + playlistTitle;
+            Directory.CreateDirectory(directory);
+            var ext = streamInfo.Container.GetFileExtension();
+            var outputFilePath = directory + "\\" + cleanTitle + ".mp3";
+            //await YoutubeClient.DownloadMediaStreamAsync(streamInfo, outputFilePath, progressHandler);
+            await FfmpegCli.ExecuteAsync($"-i \"{streamFilePath}\" -q:a 0 -map a \"{outputFilePath}\" -y");
+
+            File.Delete(streamFilePath);
+
+            // Edit mp3 metadata
+            var idMatch = Regex.Match(video.Title, @"^(?<artist>.*?)-(?<title>.*?)$");
+            var artist = idMatch.Groups["artist"].Value.Trim();
+            var titleAfter = idMatch.Groups["title"].Value.Trim();
+            try
+            {
+                using (var meta = TagLib.File.Create(outputFilePath))
+                {
+                    meta.Tag.Performers = new[] { artist };
+                    meta.Tag.Title = titleAfter;
+                    meta.Save();
+                }
+            }
+            catch
+            {
+
+            }
+
+            stateLabel.Text = "Udało się!";
+            if (!isFromPlaylist)
+                MessageBox.Show("Zakończono pobieranie utworu", "Zakończono");
+            progressBar.Value = 0;
+            
+        }
+
+        private async Task SavePlaylist(string id)
+        {
+            // Get playlist info
+            var playlist = await YoutubeClient.GetPlaylistAsync(id);
+
+            // Clear error list on new playlist
+            errorList.Clear();
+
+            // Work on the videos
+            int index = 1;
+            foreach (var video in playlist.Videos)
+            {
+                try
+                {
+                    await SaveTrackAudioAsync(video.Id, index, playlist.Videos.Count, playlist.Title, true);
+                }
+                catch
+                {
+                    //MessageBox.Show($"Niestety wystąpił błąd :( Z utworu: {video.Title} nic nie będzie :(");//tutaj sie nie udalo
+                    errorList.Add(video.Title);
+                }
+                index++;
+            }
+
+            MessageBox.Show("Zakończono pobieranie playlisty", "Zakończono");
+            if (errorList.Count != 0)
+            {
+                string titles = "";
+                foreach (var title in errorList)
+                {
+                    titles += title + "\n";
+                }
+                titles = titles.Trim();
+                MessageBox.Show("Niestety nie udało się pobrać następujących tytułów: \n" + titles, "Błąd w pobieraniu");
+            } 
+        }
+
+        private void pasteButton_Click(object sender, EventArgs e)
+        {
+            urlTextBox.Text = Clipboard.GetText();
+        }
+
+        private void urlTextBox_TextChanged(object sender, EventArgs e)
+        {
+            stateLabel.Text = "OK! :) Naciśnij przycisk \"Sciągnij\"";
+        }
+
+        private void folderExplorerButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start(@chooseTextBox.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Folder jeszcze nie istnieje. Zostanie utworzony wraz z pierwszym pobranym utworem.", "Błąd");
+            }
+            
+        }
+
+        private void YTDownloader_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.IO.DirectoryInfo di = new DirectoryInfo(TempDirectoryPath);
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+        }
+    }
+}
+ 
